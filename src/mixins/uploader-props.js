d8d3d1491e0f4d8a0fe1d4c9ba2538b07d16c872
Gennady Grishkovtsov <grishkovelli@gmail.com>
2018-12-19 01:41:36 +0300

    Fix HTTP headers default value

--- src/mixins/uploader-props.js
+++ src/mixins/uploader-props.js
@@ -1,7 +1,7 @@
 export default {
   props: {
-    filename  : { type: String, default: 'record' },
-    headers   : { type: Object                    },
-    uploadUrl : { type: String                    }
+    filename  : { type: String, default: 'record'   },
+    headers   : { type: Object, default: () => ({}) },
+    uploadUrl : { type: String                      }
   }
 }
